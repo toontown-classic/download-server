69525d1bbfcce256e75efbca508c2a0907b6fc8b
anythingtechpro <anythingtechpro@gmail.com>
2018-11-19 20:05:28 -0500

    initial file commit

---
+++ htdocs/constants.php
@@ -0,0 +1,5 @@
+<?php
+  $ACCOUNT_SERVER_CONSTANTS = array('ACCOUNT SERVER CONSTANTS', 'minNameLength=4', 'minPwLength=6', 'allowNewAccounts=1', 'freeTrialPeriodInDays=700','creditCardUpFront=0', 'priceFirstMonth=9.95', 'customerServicePhoneNumber=(877)-560-6477', 'pricePerMonth=7.95');
+  list($HEADER, $FIELD_1, $FIELD_2, $FIELD_3, $FIELD_4, $FIELD_5, $FIELD_6, $FIELD_7, $FIELD_8) = $ACCOUNT_SERVER_CONSTANTS;
+  echo $HEADER."\n".$FIELD_1."\n".$FIELD_2."\n".$FIELD_3."\n".$FIELD_4."\n".$FIELD_5."\n".$FIELD_6."\n".$FIELD_7."\n".$FIELD_8;
+?>
